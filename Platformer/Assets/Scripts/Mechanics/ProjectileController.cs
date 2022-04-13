3b31dfdf7ffac40d043a2fd091105ad97d6edd06
Greg Back <greg@playstudios.com>
2022-04-12 17:34:56 -0700

    repo start

---
+++ Platformer/Assets/Scripts/Mechanics/ProjectileController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls Creation of Projectiles fired by the Player
+/// </summary>
+public class ProjectileController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
